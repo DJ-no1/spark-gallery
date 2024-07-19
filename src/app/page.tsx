4488ecc86d4afee6c58ec4ff831c78dfc53f6b89
DJ-no1 <cseai2023011@rcciit.org.in>
2024-07-19 19:54:26 +0530

    abc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Spark Gallery</h1>
-      <Link href={'/signup'}>
+      <Link href={'/sign-up'}>
       <Button variant={"default"} className=" flex justify-center px-4" >Click me</Button></Link>
     </div>
   );
